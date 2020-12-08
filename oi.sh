3d230ff8a85c890facda6c398a350fa59e7db916
Carlos Oliveira <carlos157oliveira@gmail.com>
2020-12-08 18:37:39 -0300

    Version 1.5.0

--- oi.sh
+++
@@ -1,6 +0,0 @@
-COMMIT=OI
-sed -E \
-	-e "s/\"tag\": \"v[0-9]+\.[0-9]+\.[0-9]+\",/\"tag\": \"v$VERSION\",/" \
-	-e "s/\"commit\": \"\w+\"/\"commit\": \"$COMMIT\"/" \
-	"com.github.carlos157oliveira.Calculus.json"
-echo "$COMMIT"
